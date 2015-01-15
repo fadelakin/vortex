766168e7746801905eebd7344a28e95b98603a68
Fisher Adelakin <temidayoadelakin@gmail.com>
2015-01-14 23:58:21 -0500

    handling errors

---
+++ app/src/main/java/com/fisheradelakin/vortex/AlertDialogFragment.java
@@ -0,0 +1,24 @@
+package com.fisheradelakin.vortex;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+
+/**
+ * Created by Fisher on 1/14/15.
+ */
+public class AlertDialogFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                .setTitle(context.getString(R.string.error_title))
+                .setMessage(context.getString(R.string.error_message))
+                .setPositiveButton(context.getString(R.string.error_ok_button_text), null);
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
+}
--- app/src/main/java/com/fisheradelakin/vortex/MainActivity.java
+++ app/src/main/java/com/fisheradelakin/vortex/MainActivity.java
@@ -80,13 +80,22 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onResponse(Response response) throws IOException {
                 try {
+                    Log.v(TAG, response.body().string());
                     if(response.isSuccessful()) {
-                        Log.v(TAG, response.body().string());
+                    } else {
+                        alertUserAboutError();
                     }
                 } catch (IOException e) {
                     Log.e(TAG, "Exception caught: ", e);
                 }
             }
         });
+
+        Log.d(TAG, "Main UI code is running!");
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 }
