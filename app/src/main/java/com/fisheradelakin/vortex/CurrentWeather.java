0349130651f5ee76756e0de7169d1fd78779ac16
Fisher Adelakin <temidayoadelakin@gmail.com>
2015-01-15 20:13:47 -0500

    set weather icon

--- app/src/main/java/com/fisheradelakin/vortex/CurrentWeather.java
+++ app/src/main/java/com/fisheradelakin/vortex/CurrentWeather.java
@@ -33,6 +33,49 @@ public class CurrentWeather {
         mIcon = icon;
     }
 
+    public int getIconId() {
+        // clear-day, clear-night, rain, snow, sleet, wind, fog, cloudy, partly-cloudy-day, or partly-cloudy-night
+        int iconId;
+
+        switch (mIcon) {
+            case "clear-day":
+                iconId = R.drawable.clear_day;
+                break;
+            case "clear-night":
+                iconId = R.drawable.clear_night;
+                break;
+            case "rain":
+                iconId = R.drawable.rain;
+                break;
+            case "snow":
+                iconId = R.drawable.snow;
+                break;
+            case "sleet":
+                iconId = R.drawable.sleet;
+                break;
+            case "wind":
+                iconId = R.drawable.wind;
+                break;
+            case "fog":
+                iconId = R.drawable.fog;
+                break;
+            case "cloudy":
+                iconId = R.drawable.cloudy;
+                break;
+            case "partly-cloudy-day":
+                iconId = R.drawable.partly_cloudy;
+                break;
+            case "partly-cloudy-night":
+                iconId = R.drawable.cloudy_night;
+                break;
+            default:
+                iconId = R.drawable.clear_day;
+                break;
+        }
+
+        return iconId;
+    }
+
     public long getTime() {
         return mTime;
     }
@@ -41,9 +84,8 @@ public class CurrentWeather {
         SimpleDateFormat formatter = new SimpleDateFormat("h:mm a");
         formatter.setTimeZone(TimeZone.getTimeZone(getTimeZone()));
         Date dateTime = new Date(getTime() * 1000);
-        String timeString = formatter.format(dateTime);
 
-        return timeString;
+        return formatter.format(dateTime);
     }
 
     public void setTime(long time) {
