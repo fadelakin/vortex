a675371ddd0f250a727446c077ac94115ef23ff2
Fisher Adelakin <temidayoadelakin@gmail.com>
2015-01-15 22:10:40 -0500

    plug in data

--- app/src/main/java/com/fisheradelakin/vortex/CurrentWeather.java
+++ app/src/main/java/com/fisheradelakin/vortex/CurrentWeather.java
@@ -92,8 +92,8 @@ public class CurrentWeather {
         mTime = time;
     }
 
-    public double getTemperature() {
-        return mTemperature;
+    public int getTemperature() {
+        return (int) Math.round(mTemperature);
     }
 
     public void setTemperature(double temperature) {
@@ -108,8 +108,9 @@ public class CurrentWeather {
         mHumidity = humidity;
     }
 
-    public double getPrecipChance() {
-        return mPrecipChance;
+    public int getPrecipChance() {
+        double precipPercentage = mPrecipChance * 100;
+        return (int) Math.round(precipPercentage);
     }
 
     public void setPrecipChance(double precipChance) {
