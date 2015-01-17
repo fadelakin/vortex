ad60f4c6e8f28c6065668e5d95c14e2ff06b39a9
Fisher Adelakin <temidayoadelakin@gmail.com>
2015-01-17 03:58:52 -0500

    just use network instead of gps

---
+++ app/src/main/java/com/fisheradelakin/vortex/GPSDialogFragment.java
@@ -0,0 +1,27 @@
+package com.fisheradelakin.vortex;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+/**
+ * Created by Fisher on 1/17/15.
+ */
+public class GPSDialogFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.enable_GPS)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        return builder.create();
+    }
+}
